de24e682e7056f78b681fc6b449f8c40c8f3cec9
EsipenkoAnna <annette160893@mail.ru>
2022-01-28 15:08:43 +0300

    123

--- hw1_esipenko/main.py
+++ hw1_esipenko/main.py
@@ -1,1 +1,16 @@
-print ('hihih')
+duration = 400153
+
+if duration < 60: # 1 минута = 60 секунд
+    print (duration, 'сек')
+elif 3600 > duration >= 60:  # 1 минута = 60 секунд
+    minutes = duration//60
+    seconds = duration%60
+    print(minutes, 'мин', seconds, 'сек')
+elif 86400> duration >= 3600:  # 1 час = 3600 секунд, 1 день = 86400 секунд
+    hours = duration//3600
+    minutes = (duration-(hours*3600))%60
+    seconds= duration%60
+    print(hours, 'часов', minutes, 'минуты', seconds, 'секунды')
+if 2629743 > duration > 86400:  # 1 день =604800 секунд
+    days = duration//86400
+    print (days)
