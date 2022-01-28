4ba728c85ff512da85aab1284b82e0439b9b04f3
EsipenkoAnna <annette160893@mail.ru>
2022-01-28 22:15:15 +0300

    test

--- hw1.2_esipenko/main.py
+++ hw1.2_esipenko/main.py
@@ -1,1 +1,19 @@
-print ("lalala")
+my_list = []
+for value in range (1, 1000, 2):
+    my_list.append(value**3)
+print(my_list)
+main_summ = 0
+for num in range(len(my_list)):
+    sum = 0
+    val = my_list[num]
+    print (val)
+
+    while(val!=0):
+        sum = sum + val%10
+        val=val//10
+        print(sum)
+
+   ## if sum%7==0:
+   ##     result =+main_summ
+  ##  print(main_summ)
+
