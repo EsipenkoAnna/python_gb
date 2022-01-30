26bb5a8651b8484de0954efc9902d7e04e63313b
EsipenkoAnna <annette160893@mail.ru>
2022-01-30 15:49:42 +0300

    final result

--- hw1.2_esipenko/main.py
+++ hw1.2_esipenko/main.py
@@ -1,19 +1,34 @@
 my_list = []
+new_my_list =[]
+
 for value in range (1, 1000, 2):
     my_list.append(value**3)
 print(my_list)
-main_summ = 0
-for num in range(len(my_list)):
-    sum = 0
-    val = my_list[num]
-    print (val)
 
-    while(val!=0):
-        sum = sum + val%10
-        val=val//10
-        print(sum)
+def sum_numbers(numb):
+    sum_n = 0
+    while numb != 0:
+        sum_n += numb%10
+        numb=numb//10
+        return sum_n
 
-   ## if sum%7==0:
-   ##     result =+main_summ
-  ##  print(main_summ)
+def sum_list (dataset:list)->int:
+    sum1 = 0
+    list2 = []
+    for i in dataset:
+        a=sum_numbers(i)
+        if a%7==0:
+            list2.append(i)
+    for i in list2:
+        sum1 += i
+    return sum1
 
+for value in my_list:
+    new_value=value+17
+    new_my_list.append(new_value)
+
+
+result = sum_list (my_list)
+print(result)
+result1 = sum_list (new_my_list)
+print(result1)
